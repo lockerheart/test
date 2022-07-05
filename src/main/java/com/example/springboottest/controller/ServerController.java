46e1e4e9db0c0b2c7fd26a845de2b6e6da889d68
奇迹时代 <2902009304@qq.com>
2022-07-05 13:12:10 +0800

    test

---
+++ src/main/java/com/example/springboottest/controller/ServerController.java
@@ -0,0 +1,21 @@
+package com.example.springboottest.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+@RequestMapping("/user")
+@Controller
+public class ServerController {
+    @RequestMapping("/query")
+    @ResponseBody
+    public String doSome(){
+        System.out.println("执行了query");
+        return null;
+    }
+    @RequestMapping("/login")
+    @ResponseBody
+    public String doSomelogin(){
+        System.out.println("执行了login");
+        return null;
+    }
+}
